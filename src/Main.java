060f7c71d3593db376d63a9ee576e89831ec0912
Роман <roninmtn@gmail.com>
2025-07-23 12:36:38 +0400

    Фраза №1 Я читос, он сливается!

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+// Задача №1
         byte age = 19;
         if ( age >=18){
             System.out.println("Вы достигли совершеннолетия");
@@ -14,13 +15,14 @@ public class Main {
         }else{
             System.out.printf("На улице "+temperature+" градусов, надо надеть шапку  ");
         }
-
+// Задача №2
         byte speed = 70;
         if ( speed > 60){
             System.out.printf("  Ваша скорость "+speed+" километров в час, прийдётся заплатить штраф   ");
         }else{
             System.out.printf("Ваша скорость "+speed+" километров в час можно ехать спокойно");
         }
+// Задача №3
         byte peopleAge = 14;
         if (peopleAge >=2 && peopleAge <=6){
             System.out.printf("Ваш возраст "+peopleAge+" вы должны ходить в детский сад");
@@ -34,6 +36,7 @@ public class Main {
         if (peopleAge >24){
             System.out.printf("Ваш возраст "+peopleAge+" скорее всего вы уже ходите на работу");
         }
+// Задача №4
         byte agePeople = 16;
         if (agePeople <=5){
             System.out.printf("   Ваш возраст "+agePeople+" вы не можете кататся на этом атракционе");
@@ -44,7 +47,36 @@ public class Main {
         if (agePeople >14){
             System.out.printf("   Ваш возраст "+agePeople+" вы можете кататся на этом аттракционе");
         }
-        byte people = 40;
+// Задача №5
+        byte van = 102;
+        byte vanSit = 60;
+        byte vanStand = (byte) (van-vanSit);
+// Задача №6
+        byte traveler = 100;
+        if (traveler<=vanSit){
+            System.out.printf("В вагоне остались как сидячие так и стоячие места");
+        }
+        if (traveler>vanSit && traveler<=van){
+            System.out.printf("В вагоне остались только сидячие места");
+        }
+        if (traveler>van){
+            System.out.printf("В вагоне не осталось свободных мест");
+        }
+// Задача №7
+        int one;
+        int two;
+        int three;
+        one=1;
+        two=2;
+        three=3;
+        if (one>two){
+            System.out.printf("   one");
+        }
+        if (two>three){
+            System.out.printf("   two");
+        }else{
+            System.out.printf("  three");
+        }
 
 
     }
